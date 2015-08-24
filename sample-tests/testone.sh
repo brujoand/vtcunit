7557cfda72463e5915e160a8969f67ed228ee9ad
brujoand <anders@brujordet.no>
2015-08-24 12:35:11 +0200

    Adding the sample tests

---
+++ sample-tests/testone.sh
@@ -0,0 +1,10 @@
+function test_that_we_can_match_substring() {
+  string="This is a long string"
+  substring="string"
+  assert_is_substring_of "$substring" "$string"
+}
+
+function test_that_we_can_assert_non_empty() {
+  string="heioghopp"
+  assert_none_empty "$string"
+}
