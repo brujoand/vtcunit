1878c46566ef22965ee667d4211f8399573e293c
brujoand <anders@brujordet.no>
2015-09-18 15:00:15 +0200

    Rewriting bauta, to vtcunit. only run *.vtc files, same output

--- sample-tests/theoddtest.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-function test_that_we_can_have_newlines() {
-  lang=$(echo -e "Some weird Runtim Exception:\nthis one has newlines, many\n new lines\n yes many\n hoho")
-  assert_equals "$lang" "$lang"
-}
